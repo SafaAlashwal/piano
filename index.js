a86e0ae1bf102cb9be3639ec32a40eb3a857f5a9
SafaAlashwal <136381903+SafaAlashwal@users.noreply.github.com>
2023-09-02 22:22:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// document.getElementById("chick").click()
+
+// function changelink() {
+//     var a = document.getElementById("o");
+//     a.setAttribute('href', "www.facebook.com");
+// }
+
+
+function play(num) {
+    var audio = new Audio("audio/note" + num + ".wav");
+    audio.play();
+    // if (sound == 'do') {
+    //     var audio = new Audio("audio/note1")
+    // }
+    // else if (sound = 're') { var audio = new Audio("audio/note2") }
+    // else if (sound = 'me') { var audio = new Audio("audio/note3") }
+    // else if (sound = 'fa') { var audio = new Audio("audio/note4") }
+    // else if (sound = 'so') { var audio = new Audio("audio/note5") }
+    // else if (sound = 'la') { var audio = new Audio("audio/note6") }
+    // else if (sound = 'se') { var audio = new Audio("audio/note7") }
+
+
+
+}
